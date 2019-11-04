2aed119df9206e1babbc50dfcab2903636f54b1f
shoonkim <shoonkim@khu.ac.kr>
2019-11-04 20:28:45 +0900

    2019_11_04 Fire_Database get

--- app/src/main/java/com/shoonkim/legiomariae/WeekCalenderActivity.kt
+++ app/src/main/java/com/shoonkim/legiomariae/WeekCalenderActivity.kt
@@ -1,14 +1,17 @@
 package com.shoonkim.legiomariae
 
+import android.annotation.SuppressLint
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.os.Handler
 import android.os.Message
 import android.util.Log
+import com.google.firebase.firestore.FirebaseFirestore
+import com.google.firebase.firestore.FirebaseFirestoreSettings
 import kotlinx.android.synthetic.main.week_calender_activity.*
 import java.util.*
 
-const val TAG:String = "WEEKCALENDERACTIVITY_STATRT"
+const val WCATAG:String = "WEEKCALENDERACTIVITY_STATRT"
 const val WEEKCALENDERACTIVITY_STATRT = 101
 
 class WeekCalenderActivity : AppCompatActivity() {
@@ -16,6 +19,8 @@ class WeekCalenderActivity : AppCompatActivity() {
     var list = arrayListOf<WeekCalenderData>()
     var wca_adapter : WeekCalenderListAdapter? = null
     var wcaHandler : WCAHandler? = null
+    var wca_db : FirebaseFirestore? = null
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -40,15 +45,47 @@ class WeekCalenderActivity : AppCompatActivity() {
         }
     }
 
-    fun initList(){
+    inner class wca_db_setup() : Thread(){
+        @SuppressLint("LongLogTag")
+        override fun run() {
+            wca_db = FirebaseFirestore.getInstance()
+            val settings = FirebaseFirestoreSettings.Builder()
+                .setPersistenceEnabled(true)
+                .build()
+            wca_db!!.firestoreSettings = settings
 
+            Log.d(WCATAG,wca_db.toString())
+
+            val DocumentRef = wca_db!!.collection("Calender")
+                .document("201911")
+
+            DocumentRef.collection("Day")
+                .get()
+                .addOnSuccessListener { result ->
+                    for (document in result) {
+                        Log.d(WCATAG, "${document.id} => ${document.data}")
+                    }
+                }
+                .addOnFailureListener { exception ->
+                    Log.w(WCATAG, "Error getting documents.", exception)
+                }
+        }
+    }
+
+    @SuppressLint("LongLogTag")
+    fun initList(){
+        /*
         val instance = Calendar.getInstance()
         val curYear = instance.get(Calendar.YEAR).toString()
-        val curMonth = instance.get(Calendar.MONTH).toString()
+        val curMonth = (instance.get(Calendar.MONTH).toInt() + 1).toString()
         val curDay = instance.get(Calendar.DATE).toString()
         val carWeek = instance.get(Calendar.DAY_OF_WEEK).toString()
 
-        Log.d(TAG, curYear + ":" + curMonth +":"+curDay+":"+carWeek)
-    }
 
+        Log.d(WCATAG, curYear + ":" + curMonth +":"+curDay+":"+carWeek)
+         */
+
+        val bd_setup = wca_db_setup()
+        bd_setup.start()
+    }
 }
