011a9b7a33ca3887e8c6e7753bf1861005c66aec
shoonkim <shoonkim@khu.ac.kr>
2020-02-14 12:31:32 +0900

    Reconstruction 20200213 • Login, SignUp UI 구현 중   - google Login 구현 중

--- app/src/main/java/com/shoonkim/legiomariae/ui/user/Login.kt
+++ app/src/main/java/com/shoonkim/legiomariae/ui/user/Login.kt
@@ -1,16 +1,30 @@
 package com.shoonkim.legiomariae.ui.user
 
+import android.content.Intent
 import android.os.Bundle
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import com.google.android.gms.auth.api.Auth
+import com.google.android.gms.auth.api.signin.GoogleSignIn
+import com.google.android.gms.auth.api.signin.GoogleSignInClient
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions
+import com.google.firebase.auth.FirebaseAuth
 import com.shoonkim.legiomariae.MainActivity
 import com.shoonkim.legiomariae.R
 import com.shoonkim.legiomariae.ui.RxBaseFragment
 import com.shoonkim.legiomariae.utils.inflate
 import kotlinx.android.synthetic.main.login.*
 
-class Login  : RxBaseFragment() {
+class Login  : RxBaseFragment(), View.OnClickListener {
+
+    companion object {
+        private const val RC_SIGN_IN = 9001
+    }
+
+    private lateinit var googleSignInClient: GoogleSignInClient
+    private lateinit var auth: FirebaseAuth
 
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -23,8 +37,47 @@ class Login  : RxBaseFragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
-        signupbtn.setOnClickListener {
-            (activity as MainActivity).changeFragment(SignUp())
+        login_google.setOnClickListener(this)
+        signupbtn.setOnClickListener(this)
+    }
+
+    override fun onClick(v: View?) {
+        //TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+
+        when(v?.id){
+            R.id.login_google->onLoginGoogle()
+            R.id.signupbtn -> (activity as MainActivity).changeFragment(SignUp())
         }
     }
+
+     override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+
+        when(requestCode){
+            RC_SIGN_IN->{
+                val result = Auth.GoogleSignInApi.getSignInResultFromIntent(data)
+                if(result.isSuccess){
+                    Log.d("Firebase_Auth", "google sign ok")
+                    //result.signInAccount.email
+                }else{
+                    Log.d("Firebase_Auth", "google sign not ok")
+                }
+            }
+        }
+    }
+
+    fun onLoginGoogle(){
+
+        val gso = GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
+            .requestIdToken(getString(R.string.default_web_client_id))
+            .requestEmail()
+            .build()
+
+        googleSignInClient = GoogleSignIn.getClient((activity as MainActivity), gso)
+        auth = FirebaseAuth.getInstance()
+
+        val signInIntent = googleSignInClient.signInIntent
+        startActivityForResult(signInIntent, RC_SIGN_IN)
+    }
+
 }
